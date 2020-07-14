12da0bb982859c8998ff0693c27b487c167e6c18
arihant-2310 <arihant2310@gmail.com>
2020-07-14 17:12:21 +0530

    save user to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,19 +1,31 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./keys");
+const User = require("../models/user-model");
 
 passport.use(
   new GoogleStrategy(
     {
-      //options for google strat
-      callbackURL: "/auth/google/redirect",
+      // options for google strategy
       clientID: keys.google.clientID,
       clientSecret: keys.google.clientSecret,
+      callbackURL: "/auth/google/redirect",
     },
     (accessToken, refreshToken, profile, done) => {
-      //passport callback function
-      console.log(profile);
-      console.log("passport callback called!");
+      User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          console.log("user is:", currentUser);
+        } else {
+          new User({
+            googleId: profile.id,
+            username: profile.displayName,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("new user created: ", newUser);
+            });
+        }
+      });
     }
   )
 );
